2e4416a134439da9ee20b96fab6efb1cb4fe2e84
Nayan Chandra Datta <nayanchdatta011@gmail.com>
2022-02-18 11:41:47 +0600

    Navbar updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,7 @@
 // Navabar Toggle 
 const toggleBtn = document.querySelector('.navbar__btn');
 const navItems = document.querySelector('.nav__items');
-const navMob = document.querySelector('.navbar__mobile');
 
 toggleBtn.addEventListener('click', () => {
-    navItems.classList.toggle('navbar__mobile');
+    navItems.classList.toggle('show');
 });
